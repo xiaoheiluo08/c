feb3411e0a838d3806a26de71eb4066b9c923811
xiaoheiluo08 <3204395043@qq.com>
2025-11-06 15:56:10 +0800

    Initial commit

---
+++ str_handle.c
@@ -0,0 +1,8 @@
+#include "str_handle.h"
+
+char * str_copy(char *str1,char *str2)
+{
+
+    strcpy(str1,str2);
+    
+}
---
+++ str_handle.h
@@ -0,0 +1,11 @@
+#ifndef STR_HANDLE_H
+#define STR_HANDLE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char *str_copy(char *str1,char *str2);
+
+
+#endif
